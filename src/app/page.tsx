2d90303e29b06e13f257a8263e4f095aab95e9e4
Colten Van Tussenbrook <coltenvantussenbrook@gmail.com>
2023-05-11 11:47:03 -0600

    basic data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+const getData = async () => {
+    const res = await fetch(`https://waterservices.usgs.gov/nwis/iv/?format=json&sites=10163000&parameterCd=00060,00065,00010&siteStatus=active&siteType=ST`);
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+   
+    // Recommendation: handle errors
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data');
+    }
+
+   
+    return res.json()
+  }
+
+const Home = async () => {
+    const data = await getData()
+    console.log(data)
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -10,3 +26,5 @@ export default function Home() {
     </main>
   )
 }
+
+export default Home

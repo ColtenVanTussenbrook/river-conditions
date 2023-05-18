735b36cf7ed83dc3af0841a3a7859700b8070dc6
Colten Van Tussenbrook <coltenvantussenbrook@gmail.com>
2023-05-18 09:58:16 -0600

    install materialui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { USGSdata } from '@/types';
+import type { USGSdata } from '@/types'
 import { RiversContainer, StateSelect } from './components'
 import styles from './page.module.css'
 
@@ -6,42 +6,44 @@ import styles from './page.module.css'
  * Returns all data for a single state
  */
 const getDataByState = async (stateCode: string) => {
-    const res = await fetch(`http://waterservices.usgs.gov/nwis/iv/?stateCd=${stateCode}&format=json`);
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-   
-    // Recommendation: handle errors
-    if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error('Failed to fetch data');
-    }
+  const res = await fetch(
+    `http://waterservices.usgs.gov/nwis/iv/?stateCd=${stateCode}&format=json`
+  )
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
 
-   
-    return res.json()
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
   }
 
-  const getDataByRiver = async (riverCode: string) => {
-    const res = await fetch(`http://waterservices.usgs.gov/nwis/iv/?site=${riverCode}&format=json`);
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-   
-    // Recommendation: handle errors
-    if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error('Failed to fetch data');
-    }
+  return res.json()
+}
 
-   
-    return res.json()
+const getDataByRiver = async (riverCode: string) => {
+  const res = await fetch(
+    `http://waterservices.usgs.gov/nwis/iv/?site=${riverCode}&format=json`
+  )
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
   }
 
-  const convertDataToUsable = (jsonResponse: USGSdata) => {
-    return jsonResponse.value.timeSeries || 'No data found'
-  }
+  return res.json()
+}
+
+const convertDataToUsable = (jsonResponse: USGSdata) => {
+  return jsonResponse.value.timeSeries || 'No data found'
+}
 
 const Home = async () => {
-    const data = await getDataByState('ny')
-    // const riverData = convertDataToUsable(data)
+  const data = await getDataByState('ny')
+  // const riverData = convertDataToUsable(data)
   return (
     <main className={styles.main}>
       <div className={styles.description}>

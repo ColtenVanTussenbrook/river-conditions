d165caca86042c79da220311f676efa0bf140f50
Colten Van Tussenbrook <coltenvantussenbrook@gmail.com>
2023-05-27 15:59:08 -0600

    conditionally render temperatute and height

--- src/app/components/RiverCard/RiverCard.tsx
+++ src/app/components/RiverCard/RiverCard.tsx
@@ -8,16 +8,19 @@ const RiverCard = ({ river }: { river: River }) => (
       <span className="font-semibold">Flow:</span>{' '}
       {river.discharge?.value ? `${river.discharge.value} cfs` : 'n/a'}
     </p>
-    <p>
-      <span className="font-semibold">River Height:</span>{' '}
-      {river.height?.value ? `${river.height.value} ft` : 'n/a'}
-    </p>
-    <p>
-      <span className="font-semibold">Temperature:</span>{' '}
-      {river.temperature?.value
-        ? `${celsiusToFarenheit(river.temperature?.value)} F`
-        : 'n/a'}
-    </p>
+    {river.height?.value && (
+      <p>
+        <span className="font-semibold">River Height:</span>{' '}
+        {`${river.height.value} ft`}
+      </p>
+    )}
+
+    {river.temperature?.value && (
+      <p>
+        <span className="font-semibold">Temperature:</span>{' '}
+        {`${celsiusToFarenheit(river.temperature?.value)} F`}
+      </p>
+    )}
   </div>
 )
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 // TODO: this may need to be looked at again...it's close, but there may be a rounding error.
 const celsiusToFarenheit = (temp: string) => {
-  const farNumber = parseInt(temp, 10) * (9 / 5) + 32
+  const farNumber = Math.round(parseInt(temp, 10) * (9 / 5) + 32)
   return farNumber.toString()
 }
 
